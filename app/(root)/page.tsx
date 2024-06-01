c49f6a7017d7fd31b6bbfc93895cf5fd15abce54
YersonFelipe243 <162071436+YersonFelipe243@users.noreply.github.com>
2024-05-31 23:01:33 -0500

    part of the onboarding an ts configurations tailwind

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+;
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="head-text text-left">Home</h1>
+    </main>
+  );
+}
