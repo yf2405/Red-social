eeddeab9636732f128a36151dbdefd13fdd6155b
YersonFelipe243 <162071436+YersonFelipe243@users.noreply.github.com>
2024-06-02 11:35:11 -0500

    create thread in this momento the data base the threads is ready, en we can send thread, an leather push "7"

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import Community from "../models/community.model";
 
 
 interface Params {
@@ -43,5 +44,19 @@ export async function updateUser({
         }
     } catch (error: any) {
         throw new Error(`Failed to create/update user: ${error.message}`);
+    }
+}
+
+
+export async function fetchUser(userId: string) {
+    try {
+        connectToDB();
+
+        return await User.findOne({ id: userId }).populate({
+            path: "communities",
+            model: Community,
+        });
+    } catch (error: any) {
+        throw new Error(`Failed to fetch user: ${error.message}`);
     }
 }
