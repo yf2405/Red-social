4a7aef98bee9b264364aaff81cc672a44889ef65
YersonFelipe243 <162071436+YersonFelipe243@users.noreply.github.com>
2024-06-29 11:02:34 -0500

    cambio en search

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,41 +1,39 @@
-import Image from "next/image";
-import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs/server";
 
-import { profileTabs } from "@/constants";
-
-import ThreadsTab from "@/components/shared/ThreadsTab";
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import UserCard from "@/components/cards/UserCard";
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
 
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
-import UserCard from "@/components/cards/UserCard";
 
-
-const Page = async () => {
+async function Page({
+    searchParams,
+}: {
+    searchParams: { [key: string]: string | undefined };
+}) {
     const user = await currentUser();
     if (!user) return null;
 
     const userInfo = await fetchUser(user.id);
     if (!userInfo?.onboarded) redirect("/onboarding");
 
-    //Fetch the user
     const result = await fetchUsers({
         userId: user.id,
-        searchString: '',
-        pageNumber: 1,
-        PageSize: 25
-    }
-
-    );
+        searchString: searchParams.q,
+        pageNumber: searchParams?.page ? +searchParams.page : 1,
+        pageSize: 25,
+    });
 
     return (
         <section>
-            <h1 className="head-text mb-10"> </h1>
-            {/*Search Bar*/}
-            <div className="mt-14 flex flex-col gap-9">
+            <h1 className='head-text mb-10'>Search</h1>
+
+            <Searchbar routeType='search' />
+
+            <div className='mt-14 flex flex-col gap-9'>
                 {result.users.length === 0 ? (
-                    <p className="no-result">No users</p>
+                    <p className='no-result'>No Result</p>
                 ) : (
                     <>
                         {result.users.map((person) => (
@@ -51,7 +49,14 @@ const Page = async () => {
                     </>
                 )}
             </div>
+
+            <Pagination
+                path='search'
+                pageNumber={searchParams?.page ? +searchParams.page : 1}
+                isNext={result.isNext}
+            />
         </section>
-    )
+    );
 }
+
 export default Page;
